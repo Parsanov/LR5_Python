5134734f02430fbb0627dab5307c2d29964c65f3
Parsanov <romanparsanov@github.com>
2026-05-18 18:11:27 +0000

    Remove all comments

--- weather/urls.py
+++ weather/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'weather'
 
 urlpatterns = [
-    path('',        views.index,   name='index'),
-    path('result/', views.result,  name='result'),
+    path('',        views.index,  name='index'),
+    path('result/', views.result, name='result'),
 ]
